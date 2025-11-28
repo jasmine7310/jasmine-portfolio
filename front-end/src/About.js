7c3b6e895fb9f4bffd2ee5ef67dc2ae0e73d264a
jasmine7310 <jf4654@nyu.edu>
2025-11-28 10:59:07 -0500

    fix about

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -31,7 +31,7 @@ export default function About() {
           </div>
           <div id="right-about-section" className="fade-in-on-load">
             <img src="./IMG_8008.GIF" alt="me!"/>
-            <p className="sen-font">Never expect your code to run smoothly the first run. Test, test, and test again. Then, revise. Rinse and repeat.</p>
+            <p className="sen-font">Test, revise, rinse and repeat.</p>
           </div>
           
       </div>
