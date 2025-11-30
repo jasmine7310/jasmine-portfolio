e9b011615252f45aa7e48cb1add45f82256c0522
jasmine7310 <jf4654@nyu.edu>
2025-11-29 21:31:18 -0500

    fix typo

--- front-end/src/Webslingerz.js
+++ front-end/src/Webslingerz.js
@@ -11,7 +11,7 @@ export default function Webslingerz() {
                 <p class="details-item">Python, Digital Ocean, Github, Git, Docker, MongoDB</p>
         </div>
         <p>Do you like birds but you wish they wouldn't fly away the moment you got near them? Do you wonder if there is a way to interact with these silly goobers at comfort of your desk? Then this game is for you!</p>
-        <p>Game developed with team using containters for web app and MongoDB. Copy the repository on Github onto your local machine and follow the instructions to run locally! Happy bird collecting!</p>
+        <p>Game developed with team using containers for web app and MongoDB. Copy the repository on Github onto your local machine and follow the instructions to run locally! Happy bird collecting!</p>
         <a href="https://github.com/jasmine7310/swe-webslingrz-bird-collector">Link to Github Repo here!</a>
         <h2>Preview</h2>
         <img alt="Bird Collecting Preview" src="./projects/webslingers.png" id="ws-img"></img>
